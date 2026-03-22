f2726b207234c9250b77ec85f6fcc52bdf419e7d
UBU11 <uvaisubu11@gmail.com>
2026-03-22 11:26:29 +0530

    removed unnecassary files

--- lib/inngest_client.py
+++ lib/inngest_client.py
@@ -1,3 +1,24 @@
 import logging
 import Inngest
-import Inngest.fast_api
+import Inngest.fastapi
+
+inngest = inngest.Inngest(
+    app_id = "Unpreactable",
+    logger=logging.getLogger("uvicorn"),
+)
+
+
+
+@inngest.client.create_function(
+    fn_id="inngest_test",
+    trigger=inggest.TriggerEvent(event="app/my_function")
+)
+
+async def ingest_test(ctx: inngest.Context) -> str:
+    ctx.logger.info(ctx.event)
+    return "done"
+
+app = FastAPI()
+
+inngest.fastapi_api.serve(app, inngest_client, [ingest_test])
+
